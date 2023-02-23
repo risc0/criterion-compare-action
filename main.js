511d027b66c52ba5aa53c358557466698c4e1bb7
Parker Thompson <parker@risczero.com>
2023-02-23 11:57:05 -0800

    Fixed var reference

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ async function main() {
   core.setOutput("stdout", myOutput);
   core.setOutput("stderr", myError);
 
-  const resultsAsMarkdown = convertToMarkdown(myOutput);
+  const resultsAsMarkdown = convertToMarkdown(myOutput, inputs.prettyName);
 
   // An authenticated instance of `@octokit/rest`
   const octokit = github.getOctokit(inputs.token);
