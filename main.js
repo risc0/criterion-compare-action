5107733d4fb72c3dfcd1b578ed8f85a8e435fe7c
imdaisyadams <daisy21murillo@gmail.com>
2023-06-23 12:52:23 -0700

    test createComment func2

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ async function main() {
       owner: contextObj.owner,
       repo: contextObj.repo,
       issue_number: contextObj.number,
-      body: ' ### TEST',
+      body: "body",
     });
     core.info(
       `Created comment id '${comment.id}' on issue '${contextObj.number}' in '${contextObj.repo}'.`
